2d6a3cd30d2a8d49df7278c3b5e9b43894c92b82
Rochelle Katzman <zelrochelle@gmail.com>
2023-02-24 10:13:34 -0500

    commit tree abab17e673f7bbbb2ef2094ed70c8e40ad79d969

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,23 @@
 def reverse_string(str)
   # type your code in here
+  # 1. return a new string with the reverse order 
+  # 3. Test cases: 
+  #   Input: cat
+  #   Output: tac 
+  # 4. Psuedocode: 
+  # convert string to an array
+  arr = str.split("")
+  # initialize a new array
+  newarr = []
+  # iterate over the string array starting from the last position
+  arr.each { |x| newarr.unshift(x)} 
+      
+  # add each letter to the new array (unshift)
+  # convert the new array into a string
+  #return the string 
+  newstr = newarr.join("")
+  # return newstr
+
 end
 
 if __FILE__ == $PROGRAM_NAME
